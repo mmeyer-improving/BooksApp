95f897602d90eb767f609416bff7e7900f393b3c
Michael Meyer <michael.meyer@improving.com>
2021-05-24 21:28:23 -0500

    Catching up

--- Books/Controllers/HomeController.cs
+++ Books/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Books.Models;
+using Books.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Books.Controllers
@@ -12,14 +15,44 @@ namespace Books.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly BookData _bookData;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, BookData bookData)
         {
             _logger = logger;
+            _configuration = configuration;
+            _bookData = bookData;
         }
+
 
         public IActionResult Index()
         {
+            var books = _bookData.AllBookData();
+
+            var vm = new HomeViewModel();
+            vm.Message = "Look at these wonderful books!";
+            vm.Books = books;
+
+            return View(vm);
+        }
+
+        public IActionResult BooksWithAjax()
+        {
+            return View();
+        }
+
+        public IActionResult BookData()
+        {
+            var books = _bookData.AllBookData();
+
+            return Json(books);
+        }
+
+        public IActionResult Book(int? id)
+        {
+            //TODO: Use the id passed and go get the book data.
+            //Use that book data to create a new view.
             return View();
         }
 
--- Books/Startup.cs
+++ Books/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Books.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,8 @@ namespace Books
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddScoped<BookData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
